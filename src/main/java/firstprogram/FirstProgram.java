a364178c1a735015415b938a9e1d42d0c700abf2
malfi <malfiraggraclan@gmail.com>
2023-02-12 16:02:33 +0100

    Initial commit

---
+++ src/main/java/firstprogram/FirstProgram.java
@@ -0,0 +1,16 @@
+package firstprogram;
+
+//konwencja nazwenictwa: z dużej litery i camel case
+//plik w którym piszemy kod źródłowy (kod programu)
+public class FirstProgram {
+    //gdzie program zaczyna swoją pracę
+    //Poczatek programu - metoda mail
+    public static void main(String[] args){
+        //wiadomośc którą chcecie wypisać na ekran
+        System.out.println("Hello World!");
+        /*
+            Co sie wydarzyło w momencie odpalenia programu
+                -kod z klasy  FirstProgram został przetłumaczony na FirstProgram.class
+         * */
+    }
+}
